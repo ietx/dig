eb96b11871349aed7dfe16f6bb8602bf478aa466
Gustavo <gustavosimone1@gmail.com>
2022-06-25 12:16:48 -0300

    r
    r

--- Player.gd
+++ Player.gd
@@ -2,10 +2,13 @@ extends TileMap
 
 var player_position = Vector2.ZERO
 var direction = Vector2.ZERO
+var sonar_array = []
+var layer1_array = []
 
 func _ready():
 	player_position = Vector2(2,2)
 	set_cell(player_position.x, player_position.y, 0)
+	break_tile()
 
 
 	pass # Replace with function body.
@@ -30,4 +33,13 @@ func move_player():
 	player_position += direction
 	set_cell(player_position.x, player_position.y, 0)
 	direction = Vector2.ZERO
+	
+func sonar():
+	sonar_array.append(get_cell(player_position.x - 1, player_position.y))
+	sonar_array.append(get_cell(player_position.x + 1, player_position.y))
+	sonar_array.append(get_cell(player_position.x - 1, player_position.y - 1))
+	
+func break_tile():
+	layer1_array = $Layer1.get_used_cells()
+	print(layer1_array)
 	
